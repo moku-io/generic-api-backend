e0b45aa0ded9c0630db23749676c8e021fa3dab7
Michele Redolfi <michele@moku.io>
2016-11-10 15:29:50 +0100

    nginx

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module GenericBackend
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.active_job.queue_adapter = :delayed_job
+
     config.i18n.available_locales = [:it, :en]
 
     config.middleware.insert_before Warden::Manager, Rack::Cors do
